7f40e968d76e3df6235465b5d4dd560975c364b2
Leon Jäger <leon@Leons-MacBook-Pro.fritz.box>
2022-05-21 23:17:13 +0200

    home page added

--- root/components/categories/categories.js
+++ root/components/categories/categories.js
@@ -1,5 +1,8 @@
 class Categories {
     onClickCallback;
+    mouseoutOfCategoryBtn = true;
+    mouseoutOfCollapse = true;
+
     constructor(onClick) {
         this.onClickCallback = onClick;
     }
@@ -11,7 +14,60 @@ class Categories {
                 // this.navigateTo(e.target.id);
                 if (this.onClickCallback)
                     this.onClickCallback(e.currentTarget.id);
-            })
+            });
+
+            const categoryBtnSel = $('.category-btn');
+
+            categoryBtnSel.on('mouseover', (btn) => {
+                console.log('OnBtn');
+                this.mouseoutOfCategoryBtn = false;
+                this.mouseoutOfCollapse = true;
+                const collapseId = btn.currentTarget.getAttribute('href');
+
+                $(collapseId).addClass('show');
+
+                $(categoryBtnSel).on('mouseout', () => {
+                    setTimeout(() => {
+                        if (this.mouseoutOfCollapse === true) 
+                            $(collapseId).removeClass('show');
+                    }, 170);
+                });
+
+                $(collapseId).on('mouseover', () => {
+                    console.log('OnCollapse');
+                    this.mouseoutOfCollapse = false;
+                    this.mouseoutOfCategoryBtn = true;
+
+
+                    $(collapseId).on('mouseout', () => {
+                        setTimeout(() => {
+                            if (this.mouseoutOfCategoryBtn === true)
+                                $(collapseId).removeClass('show');
+                        }, 170);
+                    });
+                });
+
+
+            });
+
+            // categoryBtnSel.on('mouseout', (btn) => {
+            //     const collapseId = btn.currentTarget.getAttribute('href');
+            //     this.mouseoutOfCategoryBtn = true;
+
+            //     if(this.mouseoutOfCollapse === true) 
+            //         $(collapseId).removeClass('show');
+            //     else
+            //         $(collapseId).addClass('show');
+
+            //     // setTimeout(() => {
+            //     //     $(collapseId).removeClass('show');  
+            //     // }, 300);
+            //     // console.log(btnId);
+            //     // console.log($(`div.collapse${btnId}`).html());
+            //     // $(`div.collapse${btnId}`).on('mouse');
+            // })
+            //$(btn.currentTarget.getAttribute('href')).removeClass('show');
+
         });
     }
 
