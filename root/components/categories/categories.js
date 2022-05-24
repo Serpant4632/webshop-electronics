19e80e74a3c0129cc9a0afb6089569f2e50b490a
Leon Jäger <leon@Leons-MacBook-Pro.fritz.box>
2022-05-24 11:00:21 +0200

    Category-bar animation bugfix

--- root/components/categories/categories.js
+++ root/components/categories/categories.js
@@ -16,59 +16,16 @@ class Categories {
                     this.onClickCallback(e.currentTarget.id);
             });
 
-            const categoryBtnSel = $('.category-btn');
+            const categoryBtn = $('.category-btn');
+            const collapse = $('#collapse-container');
+            const parentContainer = $('#categories');
 
-            categoryBtnSel.on('mouseover', (btn) => {
-                console.log('OnBtn');
-                this.mouseoutOfCategoryBtn = false;
-                this.mouseoutOfCollapse = true;
-                const collapseId = btn.currentTarget.getAttribute('href');
-
-                $(collapseId).addClass('show');
-
-                $(categoryBtnSel).on('mouseout', () => {
-                    setTimeout(() => {
-                        if (this.mouseoutOfCollapse === true) 
-                            $(collapseId).removeClass('show');
-                    }, 170);
-                });
-
-                $(collapseId).on('mouseover', () => {
-                    console.log('OnCollapse');
-                    this.mouseoutOfCollapse = false;
-                    this.mouseoutOfCategoryBtn = true;
-
-
-                    $(collapseId).on('mouseout', () => {
-                        setTimeout(() => {
-                            if (this.mouseoutOfCategoryBtn === true)
-                                $(collapseId).removeClass('show');
-                        }, 170);
-                    });
-                });
-
-
+            categoryBtn.on('mouseenter', (e) => {
+                collapse.collapse('show');
             });
-
-            // categoryBtnSel.on('mouseout', (btn) => {
-            //     const collapseId = btn.currentTarget.getAttribute('href');
-            //     this.mouseoutOfCategoryBtn = true;
-
-            //     if(this.mouseoutOfCollapse === true) 
-            //         $(collapseId).removeClass('show');
-            //     else
-            //         $(collapseId).addClass('show');
-
-            //     // setTimeout(() => {
-            //     //     $(collapseId).removeClass('show');  
-            //     // }, 300);
-            //     // console.log(btnId);
-            //     // console.log($(`div.collapse${btnId}`).html());
-            //     // $(`div.collapse${btnId}`).on('mouse');
-            // })
-            //$(btn.currentTarget.getAttribute('href')).removeClass('show');
-
+            parentContainer.on('mouseleave', (e) => {
+                collapse.collapse('hide');
+            });
         });
     }
-
 }
