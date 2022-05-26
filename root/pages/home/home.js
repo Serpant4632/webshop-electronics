ff4b924f0c44e55ec4ff2b3bad173487bc2fb072
Leon Jäger <leon@Leons-MacBook-Pro.fritz.box>
2022-05-26 20:33:57 +0200

    dynamic rendering of Product-Carousel

--- root/pages/home/home.js
+++ root/pages/home/home.js
@@ -1,20 +1,118 @@
+const products = [
+    {
+        id: '1',
+        title: 'title1',
+        description: 'asdflkasdjflka'
+    },
+    {
+        id: '2',
+        title: 'title2',
+        description: 'afdsasdflkasdjflka'
+    },
+    {
+        id: '3',
+        title: 'title3',
+        description: 'asfdasdflkasdjflhffhgka'
+    },
+    {
+        id: '4',
+        title: 'title4',
+        description: 'asdflkasdjflghfghfghfgffhghka'
+    },
+    {
+        id: '5',
+        title: 'title5',
+        description: 'asdflkasdjflghfghfghfgffhghkaasdfasdfsadfasd'
+    },
+    {
+        id: '1',
+        title: 'title1',
+        description: 'asdflkasdjflka'
+    },
+    {
+        id: '2',
+        title: 'title2',
+        description: 'afdsasdflkasdjflka'
+    },
+    {
+        id: '3',
+        title: 'title3',
+        description: 'asfdasdflkasdjflhffhgka'
+    },
+    {
+        id: '4',
+        title: 'title4',
+        description: 'asdflkasdjflghfghfghfgffhghka'
+    },
+    {
+        id: '5',
+        title: 'title5',
+        description: 'asdflkasdjflghfghfghfgffhghkaasdfasdfsadfasd'
+    },
+    {
+        id: '1',
+        title: 'title1',
+        description: 'asdflkasdjflka'
+    },
+    {
+        id: '2',
+        title: 'title2',
+        description: 'afdsasdflkasdjflka'
+    },
+    {
+        id: '3',
+        title: 'title3',
+        description: 'asfdasdflkasdjflhffhgka'
+    },
+    {
+        id: '4',
+        title: 'title4',
+        description: 'asdflkasdjflghfghfghfgffhghka'
+    },
+    {
+        id: '5',
+        title: 'title5',
+        description: 'asdflkasdjflghfghfghfgffhghkaasdfasdfsadfasd'
+    },
+];
+
+const imagesPath = '/resources/images/';
+
 class Home extends Page {
     constructor() {
         super('home');
     }
 
-    // render(parentSelector) {
-    //     $(parentSelector).load('./pages/home/home.html', () => {
-    //         $('.nav-item').on('click', (e) => {
-    //             console.log(`${e.currentTarget.id} was clicked`);
-    //             // this.navigateTo(e.target.id);
-    //             if (this.onClickCallback)
-    //                 this.onClickCallback(e.currentTarget.id);
-    //         })
-    //         // this.navigateTo('home');
-    //         // if (this.onClickCallback)
-    //         //     this.onClickCallback('home');
 
-    //     });
-    // }
+    render(parentSelector) {
+        $(parentSelector).load('./pages/home/home.html', () => {
+            $('.nav-item').on('click', (e) => {
+                console.log(`${e.currentTarget.id} was clicked`);
+                // this.navigateTo(e.target.id);
+                if (this.onClickCallback)
+                    this.onClickCallback(e.currentTarget.id);
+            })
+
+            this.renderCarouselCards(products);
+
+        });
+    }
+
+    renderCarouselCards(products) {
+
+        const productsCarouselInner = $('#products-carousel .carousel-inner');
+        const productsCarouselItem = $('#products-carousel .carousel-inner .carousel-item');
+        
+        productsCarouselItem.clone().removeClass('active').appendTo(productsCarouselInner);
+        productsCarouselItem.clone().removeClass('active').appendTo(productsCarouselInner);
+        
+        const productsCards = productsCarouselInner.find('.card');
+
+        productsCards.each(function (index) {
+            $(this).find('img').attr('src', imagesPath + 'arduino.jpeg');
+            $(this).find('.card-title').text(products[index].title);
+            $(this).find('.card-text').text(products[index].description);
+        });
+    }
 }
+ 
