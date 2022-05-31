caae59c76652e36940faccbb15bf3db748baa210
Leon Jäger <leon@Leons-MacBook-Pro.fritz.box>
2022-05-31 12:32:06 +0200

    underline category btn implemented

--- root/components/categories/categories.js
+++ root/components/categories/categories.js
@@ -19,17 +19,25 @@ class Categories {
             const categoryBtn = $('.category-btn');
             const collapse = $('#collapse-container');
 
-            categoryBtn.on('mouseenter', () => {
+            categoryBtn.on('mouseover', (e) => {
                 collapse.collapse('show');
+                $('.category-btn span').not(e.currentTarget.children[0].classList.add('category-span')).removeClass('category-span');
+                e.currentTarget.children[0].classList.add('category-span');
+                //e.currentTarget.classList.add('category-span');
+                //console.log($('.category-btn span').not($(e.currentTarget.localName))); 
+                // $('.category-btn span').not($(e.currentTarget.cl)).css('background-color', 'red');
+               // $('.category-btn span').addClass('category-span');
             });
             categoryBtn.on('click', () => {
                 collapse.collapse('toggle');
             });
             $('#navigation').on('mousemove', () => {
                 collapse.collapse('hide');
+                $('.category-btn span').removeClass('category-span');
             });
             $('#content').on('mousemove', () => {
                 collapse.collapse('hide');
+                $('.category-btn span').removeClass('category-span');
             });
         });
     }
