3d1d974c034c1cbb1be9ec314029f240ed970b25
Lukas <neopixel@web.de>
2022-06-13 15:56:19 +0200

    Hide Content if now items in local storage

--- root/pages/shopping-cart/shopping-cart.js
+++ root/pages/shopping-cart/shopping-cart.js
@@ -10,23 +10,31 @@ class ShoppingCart extends Page {
         $(parentSelector).load('./pages/shopping-cart/shopping-cart.html', () => {
             this.getShoppingCartProducts();
             // create foreach product in SC new row
-            this.scProducts.forEach((cat, index) => {
-                $('#shoppingCartTable').append(`<tr><td class="column-heading-wide">${index + 1}</td><td class="column-heading-wide">${cat['id']}</td><td class="column-heading-wide-2">${cat['title']}</td><td class="column-heading-wide">${cat['quantity']}</td><td><i class="bi bi-trash3-fill btn btn-delete-sc-item"></i></td></tr>`);
-            });
 
-            $('.btn-delete-sc-item').on('click', (e) => {
 
-                // select current index+1 from item
-                const chosenProductPosition = $(e.currentTarget.parentElement.parentElement).children(":first").html();
+            if (!this.scProducts.length) {
+                $('#table').addClass('hide-content');
 
-                // remove item by index from object
-                this.scProducts.splice(chosenProductPosition - 1, 1);
+            } else {
+                $('#table').removeClass('hide-content');
+                this.scProducts.forEach((cat, index) => {
+                    $('#shoppingCartTable').append(`<tr><td class="column-heading-wide">${index + 1}</td><td class="column-heading-wide">${cat['id']}</td><td class="column-heading-wide-2">${cat['title']}</td><td class="column-heading-wide">${cat['quantity']}</td><td><i class="bi bi-trash3-fill btn btn-delete-sc-item"></i></td></tr>`);
+                });
 
-                // update localStorage
-                localStorage.setItem('shopping-cart-products', JSON.stringify(this.scProducts));
-                console.log(this.scProducts);
+                $('.btn-delete-sc-item').on('click', (e) => {
 
-            });
+                    // select current index+1 from item
+                    const chosenProductPosition = $(e.currentTarget.parentElement.parentElement).children(":first").html();
+
+                    // remove item by index from object
+                    this.scProducts.splice(chosenProductPosition - 1, 1);
+
+                    // update localStorage
+                    localStorage.setItem('shopping-cart-products', JSON.stringify(this.scProducts));
+                    console.log(this.scProducts);
+
+                });
+            }
 
         });
     }
