1772244efca6f64e62d98b013a36555c7b6a52e6
Leon Jäger <leon@Leons-MacBook-Pro.fritz.box>
2022-06-13 19:45:53 +0200

    toast on not logged in

--- root/pages/shopping-cart/shopping-cart.js
+++ root/pages/shopping-cart/shopping-cart.js
@@ -7,15 +7,28 @@ class ShoppingCart extends Page {
 
 
     render(parentSelector) {
+        if (!sessionStorage.getItem('customerID')) {
+            var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+            var toastList = toastElList.map(function (toastEl) {
+                return new bootstrap.Toast(toastEl)
+            })
+            toastList[0].show();
+            return;
+        }
         $(parentSelector).load('./pages/shopping-cart/shopping-cart.html', () => {
             this.getShoppingCartProducts();
             // create foreach product in SC new row
 
+            $('#liveToastBtn').on('click', () => {
+
+            });
 
             if (!this.scProducts.length) {
+                $('#info-no-item-in-sc').css('display', 'block');
                 $('#table').addClass('hide-content');
 
             } else {
+                $('#info-no-item-in-sc').css('display', 'none');
                 $('#table').removeClass('hide-content');
                 this.scProducts.forEach((cat, index) => {
                     $('#shoppingCartTable').append(`<tr><td class="column-heading-wide">${index + 1}</td><td class="column-heading-wide">${cat['id']}</td><td class="column-heading-wide-2">${cat['title']}</td><td class="column-heading-wide">${cat['quantity']}</td><td><i class="bi bi-trash3-fill btn btn-delete-sc-item"></i></td></tr>`);
@@ -43,7 +56,7 @@ class ShoppingCart extends Page {
     getShoppingCartProducts() {
         this.scProducts = JSON.parse(localStorage.getItem('shopping-cart-products'));
     }
-    
+
     renderNavShoppingCart() {
         let shoppingCartProducts = this.scProducts;
         if (shoppingCartProducts) {
