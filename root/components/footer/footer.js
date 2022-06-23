bc6c5d634e9fb54a1a05f1be8e173e697438f54d
Chemtek97 <hendrik.block97@gmx.de>
2022-06-23 16:04:26 +0200

    Refactoring

--- root/components/footer/footer.js
+++ root/components/footer/footer.js
@@ -8,7 +8,6 @@ class Footer {
         $(parentSelector).load('./components/footer/footer.html', () => {
             $('.nav-item').on('click', (e) => {
                 console.log(`${e.currentTarget.id} was clicked`);
-                // this.navigateTo(e.target.id);
                 if (this.onClickCallback)
                     this.onClickCallback(e.currentTarget.id);
             });
