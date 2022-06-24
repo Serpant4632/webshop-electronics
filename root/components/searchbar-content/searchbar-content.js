38973dcf83e54945a4dec66023c9d8332fd4b8cf
Leon Jäger <leon@Leons-MacBook-Pro.fritz.box>
2022-06-24 09:55:48 +0200

    minor fix

--- root/components/searchbar-content/searchbar-content.js
+++ root/components/searchbar-content/searchbar-content.js
@@ -28,19 +28,20 @@ class SearchbarContent {
                     }
                     searchResult.forEach((value, index) => {
                         console.log(value.id);
-                        this.products = this.productDatabaseService.getProductById(value.id);
-                        console.log(this.products[index].id);
-
-                        const productsSearchItem = $('#section-shop-container .col-sm-8');
-                        productsSearchItem.clone().appendTo(productsSearchItem);
-                        const productsCards = productsSearchItem.find('.card');
-
-                        $(this).find('img').attr('src', `${imagesPath}${products[index].id}.jpeg`);
-                        console.log(this.products[index].id);
-                        $(this).find('.card-title').text(products[index].title);
-                        console.log(this.products[index].title);
-                        $(this).find('.card-text').text(products[index].description);
-                        console.log(this.products[index].description);
+                        this.productDatabaseService.getProductById(value.id).then(product => {
+                            console.log(product);
+                            
+                            const productsSearchItem = $('#section-shop-container .col-sm-8');
+                            productsSearchItem.clone().appendTo(productsSearchItem);
+                            const productsCards = productsSearchItem.find('.card');
+                            
+                            $(this).find('img').attr('src', `${imagesPath}${products[index].id}.jpeg`);
+                            console.log(this.products[index].id);
+                            $(this).find('.card-title').text(products[index].title);
+                            console.log(this.products[index].title);
+                            $(this).find('.card-text').text(products[index].description);
+                            console.log(this.products[index].description);
+                        });
                     });
                 });
             });
